3a1ae49a8de0e622101c5399dc5dea85505496f4
= <=>
2016-08-24 14:01:46 -0700

    vanilla app

---
+++ grails-app/init/user/role/sorting/Application.groovy
@@ -0,0 +1,10 @@
+package user.role.sorting
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
